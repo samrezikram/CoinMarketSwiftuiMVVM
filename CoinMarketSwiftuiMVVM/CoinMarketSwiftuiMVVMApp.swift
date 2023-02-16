5abd6139f3dcec68037260f159ad941493bafc66
samrez Ikram <samrezikram@yahoo.com>
2023-02-16 23:16:28 +0400

    Initial Commit

---
+++ CoinMarketSwiftuiMVVM/CoinMarketSwiftuiMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinMarketSwiftuiMVVMApp.swift
+//  CoinMarketSwiftuiMVVM
+//
+//  Created by Samrez Ikram on 16/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoinMarketSwiftuiMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
